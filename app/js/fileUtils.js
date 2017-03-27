37d92c67857d3ca4d1df53f517665b66069f94ec
lemon君 <lemon@diyidan.com>
2017-03-27 22:58:20 +0800

    增加问题文件

--- app/js/fileUtils.js
+++ app/js/fileUtils.js
@@ -1,10 +1,67 @@
 'user strict'
-var filePath = 'file://' + __dirname
+var path = require('path')
+// YourQuestion\resources\app\app\js
+var currentPath = __dirname
+//YourQuestion
+/*
 
-function getFilePath(){
-	return filePath
+这边不能用2个文件来存，猜测nconf只能打开一个文件流，写的时候是写在一个缓存里面的，哪个文件后写入，所有内容都写在
+后写入的那个文件了，如果用2个文件，那么有一个文件为空，没有内容。因此存问题以  问题:[答案1,答案2,答案3...]的形式存入
+存启发以：think:[think1,think2,think3..]的形式存入。问题和启发存到一个文件中
+*/
+var questionsFilePath =path.dirname(path.dirname(path.dirname(path.dirname(currentPath)))) + '/data.json'
+// var thinkFilePath =path.dirname(path.dirname(path.dirname(path.dirname(currentPath)))) + '/thinkFilePath.json'
+// var questionsFilePath = getUserHome() + '/question1.json'
+var nconf1 = require('nconf').file({file : questionsFilePath})
+// var nconf2 = require('nconf').file({file : thinkFilePath})
+
+function saveAnswer(question, answers) {
+	var answersInFile = readAnswers(question)
+	if (!answersInFile) {
+		answersInFile = new Array()
+	}
+	if (answers) {
+		answersInFile.push(answers)
+	}
+	nconf1.set(question, answersInFile)
+	nconf1.save()
+}
+
+function readAnswers(question){
+	nconf1.load()
+	return nconf1.get(question)
+}
+function svaeThink(think) {
+	var thinksInFile = readThinks()
+	if (!thinksInFile) {
+		thinksInFile = new Array()
+	}
+	if (think) {
+		thinksInFile.push(think)
+	}
+	nconf1.set('think', thinksInFile)
+	nconf1.save()
+}
+
+function readThinks(){
+	nconf1.load()
+	return nconf1.get('think')
+}
+
+function readAllQuestions(){
+	var questionObjects = nconf1.load()
+	var questions = new Array();
+	for(key in questionObjects){
+		questions.push(key)
+	}
+	return questions
 }
 
 module.exports = {
-	getFilePath : getFilePath
+	saveAnswer : saveAnswer,
+	readAnswers : readAnswers,
+	questionsFilePath : questionsFilePath,
+	readAllQuestions : readAllQuestions,
+	svaeThink : svaeThink,
+	readThinks : readThinks,
 }
--- main.js
+++ main.js
@@ -6,19 +6,63 @@ var BrowserWindow = electron.BrowserWindow
 
 var mainWindow = null;
 app.on('ready', () => {
-	var electronScreen = electron.screen
-	var size = electronScreen.getPrimaryDisplay().workAreaSize
-	var height = size.height > 1920 ? 1920 : size.height
-	var scaleRatio = height / 1920
-	var width = 1080 * scaleRatio
+    var electronScreen = electron.screen
+    var size = electronScreen.getPrimaryDisplay().workAreaSize
+    var height = size.height > 1920 ? 1920 : size.height
+    var scaleRatio = height / 1920
+    var width = 1080 * scaleRatio
 
-	mainWindow = new BrowserWindow({width : width, height : height, frame : false})
-	mainWindow.loadURL('file://' + __dirname + '/app/index.html')
+    mainWindow = new BrowserWindow({ width: width, height: height, frame: false })
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html')
+
+    // var fileUtils = require('./app/js/fileUtils')
+    // console.log("文件:" + fileUtils.filePath)
+    // var question1 = '水是怎样形成的2'
+
+    // var answers1 = fileUtils.readAnswers(question1)
+    // console.log("问题：" + question1 + "  答案：" + answers1)
+    // fileUtils.saveAnswer(question1, '水是下雨形成的2')
+    // answers1 = fileUtils.readAnswers(question1)
+    // console.log("问题：" + question1 + "  答案：" + answers1)
+
+    // fileUtils.saveAnswer('为什么会下雨', null)
+    // fileUtils.saveAnswer('为什么会有东西啊天', null)
+    // fileUtils.saveAnswer('为什么会有东西啊天', '因为自传')
+
+    // fileUtils.svaeThink('think1')
+    // fileUtils.svaeThink('think2')
+    // fileUtils.svaeThink('think3')
+    // fileUtils.svaeThink('think4')
+    // fileUtils.svaeThink('think5')
+    // fileUtils.readThinks()
+    //你先写着，用进程通信方式写，存数据取数据需要从main.js来取,见65行左右的通信及注释
+
+
+
+    // fileUtils.saveAnswer('为什么会打雷', '因为云层的摩擦')
+
+    var allQuestions = fileUtils.readAllQuestions()
+    console.log("所有问题：" + allQuestions)
 })
 
 
 /*处理程序退出问题*/
 var ipcMain = electron.ipcMain
+var fileUtils = require('./app/js/fileUtils')
 
-const {getFilePath} = require('./app/js/fileUtils')
-console.log(getFilePath())
+
+/*在你需要的js里面这么写：
+const {ipcRenderer} = require('electron')
+ipcRenderer.send('get-question', '为什么会打雷下雨')
+ipcRenderer.on('answers', (event, arg) => {
+	//arg是answer数组
+	for (answer in arg){
+		xxxx
+	}
+})
+
+*/
+ipcMain.on('get-question', function(event, arg) {
+	var answers = fileUtils.readAnswers(arg)
+    event.sender.send('answers', answers)
+})
